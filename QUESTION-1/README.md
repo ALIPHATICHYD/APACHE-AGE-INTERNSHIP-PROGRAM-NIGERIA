3ff0dfba7d5d7ef33a7bee91b209a370a15341de
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-21 15:10:11 +0100

    README text file for development and compilation

--- QUESTION-1/README.md
+++ QUESTION-1/README.md
@@ -2,8 +2,46 @@
 
 This program implements a fibonacci function using Dynamic Programming in C. It supports arithmetic operations (+, -, *, /) and conditions to compute the fibonacci sequence.
 
+## Development Environment
 
+- Operating System: macOS
+- Compiler: Xcode Command Line Tools (GCC)
 
+## How to Compile and Run
+
+1. Open Terminal.
+
+2. Navigate to the directory containing `fibonacci.c` using the `cd` command. For example, if the file is on the desktop, use the following command:
+
+   ```
+   cd ~/Desktop
+   ```
+
+3. Compile the program using the following command:
+
+   ```
+   gcc fibonacci.c -o fibonacci
+   ```
+
+   This command compiles the C source code and creates an executable named `fibonacci`.
+
+4. Run the compiled executable:
+
+   ```
+   ./fibonacci
+   ```
+
+5. The program will display the output for the given arithmetic operations and fibonacci calculation.
+
+Note: macOS comes with the Xcode Command Line Tools, which include the GCC compiler. If you don't have Xcode Command Line Tools installed, you can install them by opening Terminal and running the following command:
+
+```
+xcode-select --install
+```
+
+Once the installation is complete, you can proceed with the compilation and execution of the program.
+
+Ensure that you have a working C compiler (GCC) installed on your macOS system to compile and run the code.
 
 
 
--- QUESTION-1/fibonacci.c
+++ QUESTION-1/fibonacci.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Node {
-    TypeTag type;
-} Node;
-
 typedef enum TypeTag {
     ADD,
     MUL,
@@ -12,10 +8,17 @@ typedef enum TypeTag {
     FIBO
 } TypeTag;
 
+typedef struct Node {
+    TypeTag type;
+    struct Node* left;
+    struct Node* right;
+} Node;
 
 Node* makeFunc(TypeTag type) {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = type;
+    node->left = NULL;
+    node->right = NULL;
     return node;
 }
 
@@ -27,16 +30,16 @@ void calc(Node* node) {
 
     switch (node->type) {
         case ADD:
-            printf("add : %d\n", *((int*)node + 1) + *((int*)node + 2));
+            printf("add : %d\n", *((int*)node->left) + *((int*)node->right));
             break;
         case MUL:
-            printf("mul : %d\n", *((int*)node + 1) * *((int*)node + 2));
+            printf("mul : %d\n", *((int*)node->left) * *((int*)node->right));
             break;
         case SUB:
-            printf("sub : %d\n", *((int*)node + 1) - *((int*)node + 2));
+            printf("sub : %d\n", *((int*)node->left) - *((int*)node->right));
             break;
         case FIBO:
-            int n = *((int*)node + 1);
+            int n = *((int*)node->left);
             int fib[n + 1];
             fib[0] = 0;
             fib[1] = 1;
@@ -52,73 +55,46 @@ void calc(Node* node) {
 }
 
 int main() {
-    Node* add = (*makeFunc(ADD))(10, 6);
-    Node* mul = (*makeFunc(MUL))(5, 4);
-    Node* sub = (*makeFunc(SUB))(mul, add);
-    Node* fibo = (*makeFunc(FIBO))(sub, NULL);
-
+    Node* add = makeFunc(ADD);
+    add->left = (Node*)malloc(sizeof(Node));
+    add->right = (Node*)malloc(sizeof(Node));
+    *((int*)add->left) = 1;
+    *((int*)add->right) = 2;
     calc(add);
+
+    Node* mul = makeFunc(MUL);
+    mul->left = (Node*)malloc(sizeof(Node));
+    mul->right = (Node*)malloc(sizeof(Node));
+    *((int*)mul->left) = 3;
+    *((int*)mul->right) = 4;
     calc(mul);
+
+    Node* sub = makeFunc(SUB);
+    sub->left = (Node*)malloc(sizeof(Node));
+    sub->right = (Node*)malloc(sizeof(Node));
+    *((int*)sub->left) = 5;
+    *((int*)sub->right) = 6;
     calc(sub);
+
+    Node* fibo = makeFunc(FIBO);
+    fibo->left = (Node*)malloc(sizeof(Node));
+    *((int*)fibo->left) = 10;
     calc(fibo);
 
+    free(add->left);
+    free(add->right);
     free(add);
+
+    free(mul->left);
+    free(mul->right);
     free(mul);
+
+    free(sub->left);
+    free(sub->right);
     free(sub);
+
+    free(fibo->left);
     free(fibo);
 
     return 0;
 }
-
-
-// Node* makeAdd(Node* left, Node* right) {
-//     Node* node = makeFunc(ADD);
-//     node->left = left;
-//     node->right = right;
-//     return node;
-// }
-
-// Node* makeMul(Node* left, Node* right) {
-//     Node* node = makeFunc(MUL);
-//     node->left = left;
-//     node->right = right;
-//     return node;
-// }
-
-// Node* makeSub(Node* left, Node* right) {
-//     Node* node = makeFunc(SUB);
-//     node->left = left;
-//     node->right = right;
-//     return node;
-// }
-
-// Node* makeFibo(Node* left, Node* right) {
-//     Node* node = makeFunc(FIBO);
-//     node->left = left;
-//     node->right = right;
-//     return node;
-// }
-
-
-// int main() {
-//     Node* add = makeFunc(ADD);
-//     *((int*)add + 1) = 10;
-//     *((int*)add + 2) = 20;
-//     calc(add);
-
-//     Node* mul = makeFunc(MUL);
-//     *((int*)mul + 1) = 10;
-//     *((int*)mul + 2) = 20;
-//     calc(mul);
-
-//     Node* sub = makeFunc(SUB);
-//     *((int*)sub + 1) = 10;
-//     *((int*)sub + 2) = 20;
-//     calc(sub);
-
-//     Node* fibo = makeFunc(FIBO);
-//     *((int*)fibo + 1) = 10;
-//     calc(fibo);
-
-//     return 0;
-// }
