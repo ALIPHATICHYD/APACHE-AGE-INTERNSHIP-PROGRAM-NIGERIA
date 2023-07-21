e5699a84afd968869368587d3f9a30dbe6382c66
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-21 15:39:19 +0100

    Approach 2

--- QUESTION-2/piecewise.c
+++ QUESTION-2/piecewise.c
@@ -10,3 +10,17 @@ int recursiveFib(int n) {
         return 2;
     return recursiveFib(n - 3) + recursiveFib(n - 2);
 }
+
+// Approach 2: Dynamic Programming (Memoization) function
+int memoizedFib(int n, int* memo) {
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+    if (n == 2)
+        return 2;
+    if (memo[n] != -1)
+        return memo[n];
+    memo[n] = memoizedFib(n - 3, memo) + memoizedFib(n - 2, memo);
+    return memo[n];
+}
