21bbff61256fd710a411e95b046d13df1dd9109b
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-21 14:55:19 +0100

    Adding switch case

--- QUESTION-1/fibonacci.c
+++ QUESTION-1/fibonacci.c
@@ -18,3 +18,66 @@ Node* makeFunc(TypeTag type) {
     node->type = type;
     return node;
 }
+
+void calc(Node* node) {
+    if (node == NULL) {
+        printf("Invalid input node\n");
+        return;
+    }
+
+    switch (node->type) {
+        case ADD:
+            printf("add : %d\n", *((int*)node + 1) + *((int*)node + 2));
+            break;
+        case MUL:
+            printf("mul : %d\n", *((int*)node + 1) * *((int*)node + 2));
+            break;
+        case SUB:
+            printf("sub : %d\n", *((int*)node + 1) - *((int*)node + 2));
+            break;
+        case FIBO:
+            int n = *((int*)node + 1);
+            int fib[n + 1];
+            fib[0] = 0;
+            fib[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            printf("fibo : %d\n", fib[n]);
+            break;
+        default:
+            printf("Invalid function type\n");
+            break;
+    }
+}
+
+
+// Node* makeAdd(Node* left, Node* right) {
+//     Node* node = makeFunc(ADD);
+//     node->left = left;
+//     node->right = right;
+//     return node;
+// }
+
+// Node* makeMul(Node* left, Node* right) {
+//     Node* node = makeFunc(MUL);
+//     node->left = left;
+//     node->right = right;
+//     return node;
+// }
+
+// Node* makeSub(Node* left, Node* right) {
+//     Node* node = makeFunc(SUB);
+//     node->left = left;
+//     node->right = right;
+//     return node;
+// }
+
+// Node* makeFibo(Node* left, Node* right) {
+//     Node* node = makeFunc(FIBO);
+//     node->left = left;
+//     node->right = right;
+//     return node;
+// }
+
+
